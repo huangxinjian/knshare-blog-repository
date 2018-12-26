693c45acb301090aebbe9cc5f6d522b84488f721
huangxinjian <924168702@qq.com>
2018-12-26 11:20:34 +0800

    修改vue配置文件

--- vue.config.js
+++ vue.config.js
@@ -9,16 +9,14 @@ module.exports = {
     // 那么将这个值改为 `/my-app/`
     baseUrl: '/',
   
-    // 关闭esline规范
-    lintOnSave:false,
-  
     // 将构建好的文件输出到哪里
     outputDir: 'dist',
   
     // 是否在保存的时候使用 `eslint-loader` 进行检查。
     // 有效的值：`ture` | `false` | `"error"`
     // 当设置为 `"error"` 时，检查出的错误会触发编译失败。
-    lintOnSave: true,
+     // 设置为 fasle 关闭esline规范
+    lintOnSave: false,
   
     // 使用带有浏览器内编译器的完整构建版本
     // 查阅 https://cn.vuejs.org/v2/guide/installation.html#运行时-编译器-vs-只包含运行时
