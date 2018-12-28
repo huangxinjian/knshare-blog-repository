958a0c4ff373e2dba81ec8318319ec3456548dc5
huangxinjian <924168702@qq.com>
2018-12-28 12:55:23 +0800

    创建错误Label提示基础组件

---
+++ src/components/common/ErrorLabel.vue
@@ -0,0 +1,21 @@
+<template>
+   <label>{{errormsg}}</label>
+</template>
+
+<script>
+export default {
+    props:[
+        'errormsg'
+    ]
+}
+</script>
+
+<style scoped>
+label{
+        margin: 0;
+        border: 0;
+
+        color: red;
+        font-size: 14px;
+}
+</style>
